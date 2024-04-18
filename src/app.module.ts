55ce06e1178665a7851aafb1afafa0606505103b
Luis Urzúa <lurzua@softup.cl>
2024-04-18 17:41:24 -0400

    Despliegue listo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PagosModule } from './pagos/pagos.module';
+import { HealthCheckModule } from './health-check/health-check.module';
+
 
 
 @Module({
-  imports: [PagosModule],
+  imports: [PagosModule, HealthCheckModule],
   controllers: [],
   providers: [],
 })
